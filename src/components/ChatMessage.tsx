4cf4e1bd8f5d2942606bc3b4b16247cce780f078
Exider <Bvkube95@gmail.com>
2026-09-10 12:16:25 +0200

    Bot answer remacked with markdown + GFM

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,4 +1,6 @@
 import type { MessageBox } from '../types'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import bot from '../assets/robot.png'
 import user from '../assets/user.png'
 import './styles/ChatMessage.css'
@@ -12,7 +14,11 @@ function ChatMessage({ message, sender }: MessageBox) {
       }>
       {sender === 'bot' && <img src={bot} className="chat-message-profile" />}
       <div className="chat-message-text">
-        {message}
+        {sender === 'bot' ? (
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{message}</ReactMarkdown>
+        ) : (
+          message
+        )}
       </div>  
       {sender === 'user' && <img src={user} className="chat-message-profile" />}
     </div>
